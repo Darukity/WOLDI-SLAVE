464c83ebe1c53c3696aaeb86ab4469d5ae59cdbe
Darukity <samchgaming@gmail.com>
2024-10-07 16:41:30 +0200

    updated ping command

--- commands/utility/ping.js
+++ commands/utility/ping.js
@@ -4,7 +4,7 @@ module.exports = {
 	data: new SlashCommandBuilder()
 		.setName('ping')
 		.setDescription('Replies with Pong!'),
-	async execute(interaction) {
-		await interaction.reply('Pong!');
+	async execute(interaction, client) {
+		await interaction.reply('Slave with name ' + client.user.username + ' is alive!');
 	},
 };
